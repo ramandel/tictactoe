b30df236c832f895e0baa915efb5641afc8c9ec6
unknown <ryan_mandel@yahoo.com>
2017-11-29 23:10:31 -0800

    tweaked the win condition checker to be more efficient.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,34 +1,30 @@
-let checkForWin = function(squares, turnCounter) {
-    if (turnCounter < 4) return false;
+let checkForWin = function(squares, turnCounter, row, column) {
+    if (turnCounter < 4) return false; // there needs to be at least 4 turns prior to this one for a win condition to be possible
     let found = false;
-    //check each row for a match
-    squares.forEach((row) => {
-        if (row[0] !== '' && row[0] === row[1] && row[0] === row[2]) {
-        found = true;
-        }
-    });
-    if (found) return true;
-    // check each column for a match
-    squares[0].forEach((value, index) => {
-        if (value !== '' 
-        && squares[1][index] === value 
-        && squares[2][index] === value
-        ) {
-        found = true;
-        }
-    });
-    if (found) return true;
-    // check diagonals
-    if (squares[1][1] !== '') {
-        if (squares[0][0] === squares[1][1] 
-        && squares[2][2] === squares[1][1]
-        ) {
-        return true;
-        }
+    //check the row of the latest click for a match
+    if (
+        squares[row][0] === squares[row][1] 
+        && squares[row][0] === squares[row][2]
+    ) return true;
+    // check the row of the latest click for a match
+    if (
+        squares[0][column] === squares[1][column] 
+        && squares[0][column] === squares[2][column]
+    ) return true;
+    // check the 'forward-slash' diagonal if needed
+    if (row + column === 2) {
         if (squares[2][0] === squares[1][1] 
         && squares[0][2] === squares[1][1]
         ) {
-        return true;
+            return true;
+        }
+    }
+    // check the 'back-slash' diagonal if needed
+    if (row === column) {
+        if (squares[0][0] === squares[1][1] 
+            && squares[2][2] === squares[1][1]
+        ) {
+            return true;
         }
     }
     return false;
@@ -46,7 +42,7 @@ export default (state, action) => {
         if (state.squares[action.row][action.column] === '' && !state.victory) {
             let squares = JSON.parse(JSON.stringify(state.squares));
             squares[action.row][action.column] = state.turn;
-            let victory = checkForWin(squares);
+            let victory = checkForWin(squares, state.turnCounter, action.row, action.column);
             if (victory) {
                 return {
                     squares,
